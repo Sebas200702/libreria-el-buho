52e14777492ee8e762a8734b7965f8b66862ee02
Sebastian Torregroza <143149476+Sebas200702@users.noreply.github.com>
2026-05-12 16:27:11 +0000

    chore: add @astrojs/vercel dependency to package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,11 @@ import react from '@astrojs/react';
 import node from '@astrojs/node';
 import tailwindcss from '@tailwindcss/vite';
 
+import vercel from '@astrojs/vercel';
+
 export default defineConfig({
   output: 'server',
-  adapter: node({ mode: 'standalone' }),
+  adapter: vercel(),
   integrations: [react()],
   server: {
     host: '0.0.0.0',
